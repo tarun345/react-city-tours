7f0317e86aa526349c48e26fa38b18cf16ad928e
Tarun <taroonm345@gmail.com>
2021-05-10 18:43:19 +0530

    added bootstrap

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,41 @@
 import React from "react";
 import logo from "../../logo.svg";
-import "./navbar.scss";
+// import "./navbar.scss";
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      <img src={logo} alt="city tours company" />
-      <ul className="nav-links">
-        <li>
-          <a href="/" className="nav-link">
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="/" className="nav-link">
-            About
-          </a>
-        </li>
-        <li>
-          <a href="/" className="nav-link active">
-            Tours
-          </a>
-        </li>
-      </ul>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-3">
+      <div class="container">
+        <a class="navbar-brand" href="/">
+          <img src={logo} alt="city tours company" />
+        </a>
+        <button
+          class="navbar-toggler btn"
+          data-toggle="collapse"
+          data-target="#myid"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="myid">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="/">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/">
+                About
+              </a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="/">
+                Tours
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
